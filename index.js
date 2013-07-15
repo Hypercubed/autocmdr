ef5dc63c9430cbdfd721c882e3743f2618d24562
J. Harshbarger <hypercubed@gmail.com>
2013-07-15 12:09:22 +0900

    Updated template to only require autocmdr (for now)

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+var commander  = require('commander');
+
+/**
+ * Expose the root command.
+ */
+
+exports = module.exports = new commander.Command;
+
+/**
+ * Expose `Command`.
+ */
+
+exports.Command = commander.Command;
