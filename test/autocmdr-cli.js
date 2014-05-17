919b976fabecd7255cf7b53565b3e34c51ecb888
J. Harshbarger <hypercubed@gmail.com>
2014-05-17 21:32:42 +0900

    Changed to space indention

--- test/autocmdr-cli.js
+++ test/autocmdr-cli.js
@@ -7,73 +7,73 @@ var nixt = require('nixt');
 
 describe('autocmdr bin', function(){
 
-	var cmd = 'node '+path.join(process.cwd(), './bin/autocmdr')+' ';
-	var _cmd = [path.join(__dirname, '../bin/autocmdr'), ''];
+  var cmd = 'node '+path.join(process.cwd(), './bin/autocmdr')+' ';
+  var _cmd = [path.join(__dirname, '../bin/autocmdr'), ''];
 
-	if (process.platform === 'win32') {
-		_cmd.unshift('"' + process.execPath + '"');
-	}
+  if (process.platform === 'win32') {
+    _cmd.unshift('"' + process.execPath + '"');
+  }
 
-	cmd = _cmd.join(' ');
+  cmd = _cmd.join(' ');
 
-	function cli() {
-		return nixt({colors: false})
-			.timeout(8000)
-			.base(cmd);
-	}
+  function cli() {
+    return nixt({colors: false})
+      .timeout(8000)
+      .base(cmd);
+  }
 
-	it('--help should run without errors', function(done) {
-		cli()
-	      .run('--help')
-	      //.stdout(/Usage: autocmdr/)
-	      .stderr('')
-	      .code(0)
-	      .end(done);
-	});
+  it('--help should run without errors', function(done) {
+    cli()
+        .run('--help')
+        //.stdout(/Usage: autocmdr/)
+        .stderr('')
+        .code(0)
+        .end(done);
+  });
 
-	it('--version should run without errors', function(done) {
-		cli()
-	      .run('--version')
-	      //.stdout(/[0-9].[0-9].[0-9]/)
-	      .stderr('')
-	      .code(0)
-	      .end(done);
-	});
+  it('--version should run without errors', function(done) {
+    cli()
+        .run('--version')
+        //.stdout(/[0-9].[0-9].[0-9]/)
+        .stderr('')
+        .code(0)
+        .end(done);
+  });
 
-	it('completion should run without errors', function(done) {
-		cli()
-	      .run('completion')
-	      .stdout(/###-begin-autocmdr-completion-###/)
-	      .stdout(/_autocmdr_completion/)
-	      .stderr('')
-	      .code(0)
-	      .end(done);
-	});
+  it('completion should run without errors', function(done) {
+    cli()
+        .run('completion')
+        .stdout(/###-begin-autocmdr-completion-###/)
+        .stdout(/_autocmdr_completion/)
+        .stderr('')
+        .code(0)
+        .end(done);
+  });
 
-	it('config should run without errors', function(done) {
-		cli()
-			.run('config')
-			.stderr('')
-			.code(0)
-			.end(done);
-	});
+  it('config should run without errors', function(done) {
+    cli()
+      .run('config')
+      .stderr('')
+      .code(0)
+      .end(done);
+  });
 
-	it('should return error on unknown command', function(done) {
-		cli()
-			.run('addd')
-			//.stderr(/autocmdr --help/)
-			.code(1)
-			.end(done);
-	});
+  it('should return error on unknown command', function(done) {
+    cli()
+      .run('addd')
+      //.stderr(/autocmdr --help/)
+      .code(1)
+      .end(done);
+  });
 
-	it('should return error on missing command', function(done) {
+  it('should return error on missing command', function(done) {
 
-		cli()
-			.run('')
-			//.stderr(/No command specified/)
-			.code(1)
-			.end(done);
+    cli()
+      .run('')
+      //.stderr(/No command specified/)
+      .code(1)
+      .end(done);
 
-	});
+  });
 
 });
